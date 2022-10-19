0476c0dcccd2034aeb10df8b157e1a6a7f654856
ArthurVBS <arthurvini2703@gmail.com>
2022-10-19 19:41:25 -0300

    Remove librarian endpoints in backend

--- backend/src/main/java/com/book/repository/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.repository;
-
-import com.book.entity.Librarian;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibrarianRepository extends JpaRepository<Librarian, Long> {
-}
